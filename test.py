1b38f55c34f5c0144ab7e7c9a0fd5f18311f9dfb
AnChii33 <78897150+AnChii33@users.noreply.github.com>
2022-10-29 09:49:11 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-engine.setProperty("rate", 50)
-engine.setProperty("voice", "english")
-engine.say("I am the text spoken after changing the speech rate.")
-engine.runAndWait()
